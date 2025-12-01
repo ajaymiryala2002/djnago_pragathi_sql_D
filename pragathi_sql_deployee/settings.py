f5390a6b6ec6cdcbd64acb1bdd7ff969369509fa
ajaymiryala2002 <ajaymiryala259@gmail.com>
2025-12-01 09:27:15 +0530

    Initial Django commit

--- pragathi_sql_deployee/settings.py
+++ pragathi_sql_deployee/settings.py
@@ -74,12 +74,27 @@ WSGI_APPLICATION = 'pragathi_sql_deployee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+import os 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.environ.get("MYSQLDATABASE"),
+            "USER": os.environ.get("MYSQLUSER"),
+            "PASSWORD": os.environ.get("MYSQLPASSWORD"),
+            "HOST": os.environ.get("MYSQLHOST"),
+            "PORT": os.environ.get("MYSQLPORT", "3306"),
+        }
     }
-}
+
 
 
 # Password validation
